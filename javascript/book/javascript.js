b3c39e4380270427aa18bd0f5aadd6702404cd37
freeOdin <159131665+freeOdin@users.noreply.github.com>
2024-11-06 19:05:42 +0000

    Add skeleton files for Library project

---
+++ javascript/book/javascript.js
@@ -0,0 +1,10 @@
+function Book(title, author, pages, read) { 
+    this.title = title; 
+    this.author = author; 
+    this.pages = pages; 
+    this.read = read;
+    this.info = () => {
+        const readStatus = this.read ? "already read" : "not read yet";
+        return `${this.title}, by ${this.author}, ${this.pages} pages, ${readStatus}.`; 
+    } 
+}
